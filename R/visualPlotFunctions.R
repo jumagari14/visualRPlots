50e886c9189762bbbab1bdbe6c93447ecb1b31d4
Juan Manuel Garcia <jumagarilla@hotmail.com>
2026-02-21 12:08:02 +0100

    Minor fix of stratum object in sankey plot

--- R/visualPlotFunctions.R
+++ R/visualPlotFunctions.R
@@ -53,7 +53,7 @@ plot_sankey <- function(data, alluvium = "alluvium", x = "x",
                                      label = stratum, fill = stratum)) +
     ggalluvial::geom_flow(stat = "flow", aes.flow = "backward", color = "darkgray") +
     ggalluvial::geom_stratum(width = 0.35) +
-    ggplot2::geom_text(stat = "stratum",
+    ggplot2::geom_text(stat = ggalluvial::StatStratum,
                           ggplot2::aes(label = paste0(after_stat(stratum))),
                           size = text_size) +
     ggpubr::theme_pubr() +
