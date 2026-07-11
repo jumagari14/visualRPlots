40de2608551a112c292a149b2b6d15f506f6c39b
Juan Manuel Garcia <jumagarilla@hotmail.com>
2026-07-11 19:46:26 +0200

    Minor fix with step_increase input variable

--- R/visualPlotFunctions.R
+++ R/visualPlotFunctions.R
@@ -1180,7 +1180,7 @@ getClusteredHeatmap <- function(inFile,
       data = data,
       formula = as.formula(paste(y_var, "~", x_var)),
       step.increase = step_increase,
-      y.trans=function(x) x+quantile(x,0.75)*step.increase)
+      y.trans=function(x) x+quantile(x,0.75)*step_increase)
 
     plot <- plot + ggpubr::stat_pvalue_manual(
       posthoc_prep,
